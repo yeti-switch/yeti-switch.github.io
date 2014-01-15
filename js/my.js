bdf1ce8b57b8150937dda345e2c140a62bc9a1a0
Igor Fedoronchuk <igor.f@didww.com>
2014-01-16 01:17:51 +0200

    added features to _data

--- js/my.js
+++ js/my.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
     $('#droop').click(function(){
         $('.drop_menu').slideToggle('fast');
     });
-    $("a").click(function() {
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top + "px"
-        }, {
-            duration: 500
-        });
-        return false;
-    });
+    // $("a").click(function() {
+    //        $("html, body").animate({
+    //            scrollTop: $($(this).attr("href")).offset().top + "px"
+    //        }, {
+    //            duration: 500
+    //        });
+    //        return false;
+    //    });
+
 });
