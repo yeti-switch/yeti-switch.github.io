df3d949f00b9c45a480909de11e1729658b9a4bf
Igor Fedoronchuk <igor.f@didww.com>
2014-01-16 23:28:33 +0200

    hide js

--- js/my.js
+++ js/my.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('#droop').click(function(){
+    $('#droop').click(function() {
         $('.drop_menu').slideToggle('fast');
     });
     $(".features_nav li a").click(function() {
-           $("html, body").animate({
-               scrollTop: $($(this).attr("href")).offset().top + "px"
-           }, {
-               duration: 500
-           });
-           return false;
-       });
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500
+        });
+        return false;
+    });
+
+    var mail_anchor = $("#mail-anchor");
+    var m = new Array("team", "yeti-switch.org").join("@");
+
+    mail_anchor.attr("href", "mail" + "to:" + m);
+    mail_anchor.html(mail_anchor.html().replace('{mail}', m));
 
 });
