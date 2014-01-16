71fde6f50c5f6f16a1e68e7919ab6dd609963eee
Александр Сикорский <djjedclash@gmail.com>
2014-01-16 13:44:59 +0200

    add features page

--- js/my.js
+++ js/my.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     $('#droop').click(function(){
         $('.drop_menu').slideToggle('fast');
     });
-    // $("a").click(function() {
-    //        $("html, body").animate({
-    //            scrollTop: $($(this).attr("href")).offset().top + "px"
-    //        }, {
-    //            duration: 500
-    //        });
-    //        return false;
-    //    });
+    $(".features_nav li a").click(function() {
+           $("html, body").animate({
+               scrollTop: $($(this).attr("href")).offset().top + "px"
+           }, {
+               duration: 500
+           });
+           return false;
+       });
 
 });
