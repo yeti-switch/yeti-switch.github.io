10ce4a61a23f6c7a4b00b6780909f8a68a6faea3
Igor Fedoronchuk <igor.f@didww.com>
2014-01-17 16:10:29 +0200

    contact us

--- js/my.js
+++ js/my.js
@@ -13,9 +13,11 @@ $(document).ready(function() {
     });
 
     var mail_anchor = $("#mail-anchor");
-    var m = new Array("team", "yeti-switch.org").join("@");
+    if (typeof(mail_anchor[0])!== "undefined"){
+	    var m = new Array("team", "yeti-switch.org").join("@");
+	    mail_anchor.attr("href", "mail" + "to:" + m);
+	    mail_anchor.html(mail_anchor.html().replace('{mail}', m));
+	}
 
-    mail_anchor.attr("href", "mail" + "to:" + m);
-    mail_anchor.html(mail_anchor.html().replace('{mail}', m));
 
 });
