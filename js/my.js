5030e0273a27888598bdcd0ae5aefddd05cf2937
Igor Fedoronchuk <igor.f@didww.com>
2014-01-18 18:11:42 +0200

    js fix

--- js/my.js
+++ js/my.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
 	$('#affix-nav').affix({
 	    offset: {
-	      bottom:  function () {
+	      top: 111
+	    , bottom:  function () {
 	    	        return (this.bottom = $('.bs-footer').outerHeight(true) )
 	     	      }
 	    }
 	  })
+	// $('#affix-nav ul').affix({
+	//   offset: '100px';
+	// });
+
     $('#droop').click(function() {
         $('.drop_menu').slideToggle('fast');
     });
-   
+    // $(".features_nav li a").click(function() {
+    //         $("html, body").animate({
+    //             scrollTop: $($(this).attr("href")).offset().top + "px"
+    //         }, {
+    //             duration: 500
+    //         });
+    //     });
+
     var mail_anchor = $("#mail-anchor");
     if (typeof(mail_anchor[0])!== "undefined"){
 	    var m = new Array("team", "yeti-switch.org").join("@");
